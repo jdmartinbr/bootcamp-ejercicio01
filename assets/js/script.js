c397b2bc630866995c16d9dffc758a66fd96e7c7
jdmartinbr <jdmartinbr@users.noreply.github.com>
2018-03-04 09:54:51 +0100

    Contact section & animations

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+
+    $(window).on('scroll', function () {
+        if($(window).scrollTop()===0 && $("#menu-toggler").attr("aria-expanded")===true) {
+            $('nav').removeClass('nav-scroll');
+            $('#main-menu').addClass('menu-scroll');
+        };
+    });
+
+    $(window).on('scroll', function () {
+        if($(window).scrollTop()) {
+            $('nav').addClass('nav-scroll');
+            $('#main-menu').addClass('menu-scroll');
+        }
+        else {
+            if ($("#menu-toggler").attr("aria-expanded")==="false") {
+
+                console.log("estoy en 0")
+                $('nav').removeClass('nav-scroll');
+                $('#main-menu').removeClass('menu-scroll');
+            };
+        };
+    });
+
+
+
+/*&& !($("#menu-toggler").attr("aria-expanded"))*/
+
+    var pressed = true;
+
+
+    $("#menu-toggler").click(function () {
+        var altura = $(window).scrollTop();
+
+        if (altura === 0) {
+            if ($("#menu-toggler").attr("aria-expanded")==="false") {
+                pressed = false;
+                console.log("true")
+                $('nav').addClass('nav-scroll');
+                $('#main-menu').addClass('menu-scroll');
+            } else {
+                pressed = true;
+                $('nav').removeClass('nav-scroll');
+                $('#main-menu').removeClass('menu-scroll');
+            }
+        }
+    });
+
+    $('.material-heart').on('click', function () {
+        $(this).toggleClass('material-heart-red')
+    });
+
+    $('#heartt').on('click', function () {
+        $(this).toggleClass('material-heart-red2')
+    });
+
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop()>100) {
+            $('#totop').fadeIn(600);
+        } else {
+            $('#totop').fadeOut(600);
+        }
+    });
+
+
+    $('#totop').click(function () {
+        console.log("hola");
+        $("html, body").animate({scrollTop: 0}, 600);
+    });
+
+});
